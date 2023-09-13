a62604341679b6983e2d1283c728e627aee31592
Md. Fahim Alam <kfahim2280@gmail.com>
2023-09-13 17:30:00 +0600

    Create Project

---
+++ Quad Theory Limited/Quad Theory Limited/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quad_Theory_Limited.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
